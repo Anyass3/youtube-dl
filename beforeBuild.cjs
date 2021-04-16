5fc6c7daeb4316cf4288a584dc0e31c55b1c93b1
Anyass3 <nyassabu@gmail.com>
2021-04-16 19:00:22 +0000

    fix some bug

--- beforeBuild.cjs
+++ beforeBuild.cjs
@@ -15,8 +15,10 @@ if (
 ) {
 	let file = fs.readFileSync('src/lib/connectionBuilder.ts', 'utf-8');
 	let server_endpoint = fs.readFileSync('server_endpoint', 'utf-8');
+	server_endpoint = server_endpoint.replace(/[\n\t\ ]*/);
 	if (!(server_endpoint.includes('"') || server_endpoint.includes("'")))
 		server_endpoint = `'${server_endpoint}'`;
+
 	file = file.replace('import.meta.env.VITE_SERVER_ENPOINT', server_endpoint);
 	fs.writeFileSync('src/lib/connectionBuilder.ts', file);
 	console.log('server_endpoint is from server_endpoint', server_endpoint);
