724234b62b2a87cee91e17d5786bc19f40da9fe4
Anyass3 <nyassabu@gmail.com>
2021-12-13 13:32:25 +0000

    updated workbox

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,11 +1,18 @@
-// @ts-ignore
 import { build, files, timestamp } from '$service-worker';
 
-import { precacheAndRoute } from 'workbox-precaching';
+// @ts-ignore
+importScripts('/workbox-v6.4.2/workbox-sw.js');
 
-// // @ts-ignore
-precacheAndRoute([...build, ...files]);
+workbox.setConfig({
+	// debug: true,
+	modulePathPrefix: '/workbox-v6.4.2'
+});
 
+workbox.loadModule('workbox-precaching');
+
+workbox.precaching.precacheAndRoute([...build, ...files]);
+
+// @ts-ignore-file
 self.addEventListener('message', (event) => {
 	if (event.data && event.data.type === 'SKIP_WAITING') {
 		// @ts-ignore
